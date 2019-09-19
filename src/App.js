970cc3d088124cb0f38223df17b4ea856dbdcfcb
Gabriel Dantas <gabriel.dantas@pmm.am.gov.br>
2019-09-19 09:01:41 -0400

    Inicializando o redux e adicionando o Provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <p>Hello World</p>
+    </Provider>
+   
   );
 }
 
